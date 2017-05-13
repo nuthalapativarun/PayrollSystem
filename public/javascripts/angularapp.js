108a5c6c2977fb67373a026482296e73c30831ad
Nuthalapati Varun <nuthalapativarun@gmail.com>
2017-05-13 15:23:17 -0700

    Major changes

--- public/javascripts/angularapp.js
+++ public/javascripts/angularapp.js
@@ -1,19 +1,5 @@
 var payrollApp = angular.module('payrollApp', ['ngRoute', 'angularRoutes', 'ngCookies', 'xeditable']);
 
 payrollApp.controller('getfitController', function ($rootScope, $scope, $location) {
-    $rootScope.isLoggedIn = localStorage.getItem("isLoggedIn");
-    $scope.signout = function () {
-        if ($rootScope.isLoggedIn) {
-            localStorage.clear();
-            localStorage.setItem("isLoggedIn", false);
-            $rootScope.isLoggedIn = localStorage.getItem("isLoggedIn");
-            $location.path("/login");
-        }
-        if ($rootScope.isDoctorLoggedIn) {
-            localStorage.clear();
-            localStorage.setItem("isDoctorLoggedIn", false);
-            $rootScope.isDoctorLoggedIn = localStorage.getItem("isDoctorLoggedIn");
-            $location.path("/doctorLogin");
-        }
-    };
+
 });
