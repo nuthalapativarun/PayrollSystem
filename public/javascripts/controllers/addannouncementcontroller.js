c1359c2cea096614641c28852b4fc7cd34b5bf6b
Srinadh Krothapalli <srinadh.krothapalli@gmail.com>
2017-05-15 22:05:24 -0700

    Add employee and anouncement update

--- public/javascripts/controllers/addannouncementcontroller.js
+++ public/javascripts/controllers/addannouncementcontroller.js
@@ -1,18 +1,6 @@
 payrollApp.controller('addannouncementController', function ($scope,$http,$location,$rootScope,$window) {
     console.log("In add announcement controller");
       this.announcement = {};
-      function formatDate(date) {
-		var d = new Date(date),
-			month = '' + (d.getMonth() + 1),
-			day = '' + d.getDate(),
-			year = d.getFullYear();
-
-		if (month.length < 2) month = '0' + month;
-		if (day.length < 2) day = '0' + day;
-		this.date = [year, month, day].join('-');
-		return this.date;
-	}
-    this.announcement.aDate = formatDate($scope.date);
       this.addAnnouncement = function (){
          $http.post('/api/admin/insert/announcement', this.announcement).then(function (response) {
                 console.log("Status from Signup api %d", response.status);
